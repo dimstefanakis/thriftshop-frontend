f5dd4c8753a5151faca862158ebae3900da6b9d3
VasilisKord <vkord21@gmail.com>
2022-03-26 13:54:09 +0200

    landing page complete

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { NextUIProvider, createTheme } from "@nextui-org/react";
+import { url } from 'inspector';
+import blurr from "../public/blurrBGfilled.png";
 
 const theme = createTheme({
   type: "dark",
@@ -13,10 +15,9 @@ const theme = createTheme({
       gradient:
         "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)",
       link: "#5E1DAD",
-
+      
       // you can also create your own color
       myColor: "#ff4ecd",
-
       // ...  more colors
     },
     space: {},
--- src/flat/Landing/index.tsx
+++ src/flat/Landing/index.tsx
@@ -1,43 +1,134 @@
 import { Container, Col, Row, Text, Image } from "@nextui-org/react";
+import { url } from "inspector";
 
 function Landing() {
   return (
+    <>
+      <Container
+        css={{
+          height: "100vh",
+          minHeight: "100vh",
+          "@sm": {
+            flexWrap: "nowrap",
+          },
+        }}
+        alignItems="center"
+        justify="center"
+        display="flex"
+      >
+        <Col css={{ height: "100%" }}>
+          <Container
+            css={{ height: "100%" }}
+            justify="center"
+            alignItems="center"
+            display="flex"
+          >
+            <Text
+              h1
+              size={60}
+              css={{
+                textGradient: "45deg, $blue500 -20%, $pink500 50%",
+                maxWidth: "500px",
+              }}
+            >
+              Buy high quality pre-revenue MVPs
+            </Text>
+          </Container>
+        </Col>
+        <Col>
+          <Image src="/illustration.png" />
+        </Col>
+      </Container>
+      <Story />
+      <Vision />
+    </>
+  );
+}
+
+function Story() {
+  return (
     <Container
-      css={{
-        height: "100vh",
-        minHeight: "100vh",
-        "@sm": {
-          flexWrap: "nowrap",
-        },
-      }}
       alignItems="center"
       justify="center"
-      display="flex"
+      css={{ height: "100vh", marginTop: "100px" }}
     >
-      <Col css={{ height: "100%" }}>
-        <Container
-          css={{ height: "100%" }}
-          justify="center"
-          alignItems="center"
-          display="flex"
+      <Container display="flex">
+        <Text
+          css={{
+            fontSize: "85px",
+            fontWeight: "800",
+            letterSpacing: "0.02em",
+          }}
         >
-          <Text
-            h1
-            size={60}
-            css={{
-              textGradient: "45deg, $blue500 -20%, $pink500 50%",
-              maxWidth: "500px",
-            }}
-          >
-            Buy high quality pre-revenue MVPs
-          </Text>
-        </Container>
-      </Col>
-      <Col>
-        <Image
-          src="/illustration.png"
-        />
-      </Col>
+          The story
+        </Text>
+      </Container>
+      <Container display="flex" css={{ marginTop: "100px", width: "50%" }}>
+        <p>
+          About 90% of startups fail. What about projects that never kicked off,
+          the project that a developer started sometime somewhere but never put
+          on display? They all currently live on github graveyards. Does that
+          mean they should stay on graveyards?
+          <b> No.</b>
+        </p>
+        <p style={{ marginTop: "30px" }}>
+          Most startup founders are developers and what’s the most common
+          misconception tech founders have?{" "}
+          <span style={{ fontStyle: "italic" }}>
+            {" "}
+            If you build it they will come.
+          </span>{" "}
+          42% of startups fail due to misreading market demand and this mindset
+          is a vital cause for this rate. But what if we could save these ideas
+          through small pivots? Just look at the{" "}
+          <span style={{ borderBottom: "2px solid" }}> Slack story.</span>
+        </p>
+      </Container>
+    </Container>
+  );
+}
+
+function Vision() {
+  return (
+    <Container
+      alignItems="center"
+      justify="center"
+      css={{ height: "100vh", marginTop: "100px" }}
+    >
+      <Container display="flex">
+        <Text
+          css={{
+            fontSize: "85px",
+            fontWeight: "800",
+            letterSpacing: "0.02em",
+          }}
+        >
+          The vision
+        </Text>
+      </Container>
+      <Container display="flex" css={{ marginTop: "100px", width: "50%" }}>
+        <p>
+          What does that leave us with?
+          <b>
+            {" "}
+            The rest 90% of startups with the potential to bloom through simple
+            pivots by smart people.
+          </b>
+        </p>
+        <p>
+          <b>
+            {" "}
+            Our vision is to help tech founders not linger on MVPs that are in a
+            state of coma.
+          </b>
+        </p>
+        <p>
+          <b>
+            Our vision is to help business founders find low cost opportunities,
+            high quality MVPs with unicorn potential.
+          </b>
+        </p>
+      </Container>
     </Container>
   );
 }
