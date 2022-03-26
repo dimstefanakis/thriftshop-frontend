964be7dec1732474e9494c3e3b210ddf67124b15
Dimitris Stefanakis <jimstef@outlook.com>
2022-03-27 01:28:31 +0200

    responsive stuff

--- src/flat/Landing/index.tsx
+++ src/flat/Landing/index.tsx
@@ -5,8 +5,8 @@ function Landing() {
   return (
     <>
       <Container
+        fluid
         css={{
-          height: "100vh",
           minHeight: "100vh",
           "@sm": {
             flexWrap: "nowrap",
@@ -16,7 +16,15 @@ function Landing() {
         justify="center"
         display="flex"
       >
-        <Col css={{ height: "100%" }}>
+        <Col
+          css={{
+            height: "100%",
+            width: '100%',
+            "@xs": {
+              width: "30%",
+            },
+          }}
+        >
           <Container
             css={{ height: "100%" }}
             justify="center"
@@ -25,18 +33,41 @@ function Landing() {
           >
             <Text
               h1
-              size={60}
               css={{
-                textGradient: "45deg, $blue500 -20%, $pink500 50%",
+                // textGradient: "45deg, $blue500 -20%, $pink500 50%",
                 maxWidth: "500px",
+                minWidth: "400px",
+                lineHeight: 1,
+                fontSize: 40,
+                "@xs": {
+                  fontSize: 90,
+                },
               }}
             >
-              Buy high quality pre-revenue MVPs
+              Buy high quality pre-revenue{" "}
+              <Text
+                color="#2A21E5"
+                h1
+                css={{
+                  lineHeight: 1,
+                  fontSize: 40,
+                  "@xs": {
+                    fontSize: 90,
+                  },
+                }}
+              >
+                MVPs
+              </Text>
             </Text>
           </Container>
         </Col>
-        <Col>
-          <Image src="/illustration.png" />
+        <Col css={{ minHeight: "100vh", width: "100%" }}>
+          <Image
+            height="100vh"
+            width="100%"
+            objectFit="cover"
+            src="/illustration.png"
+          />
         </Col>
       </Container>
       <Story />
@@ -63,25 +94,32 @@ function Story() {
           The story
         </Text>
       </Container>
-      <Container display="flex" style={{ marginTop: "50px", maxWidth:"800px" }}>
-        <Text >
+      <Container
+        display="flex"
+        style={{ marginTop: "50px", maxWidth: "800px" }}
+      >
+        <Text>
           About 90% of startups fail. What about projects that never kicked off,
           the project that a developer started sometime somewhere but never put
           on display? They all currently live on github graveyards. Does that
           mean they should stay on graveyards?
           <Text b> No.</Text>
         </Text>
-        <Text css={{ marginTop: "30px"}}>
+        <Text css={{ marginTop: "30px" }}>
           Most startup founders are developers and what’s the most common
           misconception tech founders have?{" "}
-          <Text i>
+          <Text i> If you build it they will come.</Text> 42% of startups fail
+          due to misreading market demand and this mindset is a vital cause for
+          this rate. But what if we could save these ideas through small pivots?
+          Just look at the{" "}
+          <Link
+            underline="true"
+            color="text"
+            css={{ textDecoration: "underline", fontWeight: "400" }}
+          >
             {" "}
-            If you build it they will come.
-          </Text>{" "}
-          42% of startups fail due to misreading market demand and this mindset
-          is a vital cause for this rate. But what if we could save these ideas
-          through small pivots? Just look at the{" "}
-          <Link underline= "true" color="text" css={{ textDecoration:"underline",fontWeight:"400" }} > Slack story.</Link>
+            Slack story.
+          </Link>
         </Text>
       </Container>
     </Container>
@@ -107,7 +145,7 @@ function Vision() {
         </Text>
       </Container>
       <Container display="flex" css={{ marginTop: "50px", maxWidth: "820px" }}>
-        <Text >
+        <Text>
           What does that leave us with?
           <Text b>
             {" "}
@@ -115,7 +153,7 @@ function Vision() {
             pivots by smart people.
           </Text>
         </Text>
-        <Text >
+        <Text>
           <Text b>
             {" "}
             Our vision is to help tech founders not linger on MVPs that are in a
