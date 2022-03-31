c54caac65fa10ba6e86b7ab080cf852a7ab3450a
Dimitris Stefanakis <jimstef@outlook.com>
2022-03-31 05:15:23 +0300

    newsletter works

--- src/features/SubscribeToNewsletter/index.tsx
+++ src/features/SubscribeToNewsletter/index.tsx
@@ -1,6 +1,36 @@
-import { Input, Button, Container, Text } from "@nextui-org/react";
+import { useState } from "react";
+import { Input, Button, Container, Text, Loading } from "@nextui-org/react";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+import { SubscribeFormProps } from "./interface";
+
+const url =
+  "https://gmail.us14.list-manage.com/subscribe/post?u=7c8673cc550b9f24941ac5fe4&amp;id=c22e5d8fee";
 
 function SubscribeToNewsletter() {
+  return (
+    <MailchimpSubscribe
+      url={url}
+      render={({ subscribe, status, message }) => (
+        <SubscribeForm
+          status={status}
+          message={message}
+          onValidated={(formData: any) => subscribe(formData)}
+        />
+      )}
+    />
+  );
+}
+function SubscribeForm({ status, message, onValidated }: SubscribeFormProps) {
+  const [email, setEmail] = useState("");
+
+  const submit = () =>
+    email &&
+    email &&
+    email.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email,
+    });
+
   return (
     <Container
       css={{
@@ -15,11 +45,25 @@ function SubscribeToNewsletter() {
         Subscribe to our newsletter
       </Text>
       <Input
+        onChange={(e) => setEmail(e.target.value)}
         contentRightStyling={false}
         placeholder="Your email"
         contentRight={
-          <Button flat color="primary" auto>
-            Join
+          <Button
+            flat
+            color="primary"
+            auto
+            onClick={submit}
+            type="submit"
+            clickable={status !== "success"}
+          >
+            {status == "sending" ? (
+              <Loading type="spinner" color="white" size="sm" />
+            ) : status == "success" ? (
+              "Subscribed!"
+            ) : (
+              "Join"
+            )}
           </Button>
         }
       ></Input>
