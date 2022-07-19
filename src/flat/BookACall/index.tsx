5009d84b61f1eec641907594880bc36d1903e981
Dimitris <jimstef@outlook.com>
2022-07-19 23:48:45 +0300

    added book a call

---
+++ src/flat/BookACall/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/react";
+
+function BookACall() {
+  return (
+    <Button color="gradient" auto css={{ mr: "$xs" }}>
+      Book a Call
+    </Button>
+  );
+}
+
+export default BookACall;
