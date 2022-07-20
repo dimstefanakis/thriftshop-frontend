37ad4747b6678d67bcc444ba2098956a56bb06d0
Dimitris <jimstef@outlook.com>
2022-07-20 19:46:04 +0300

    added book link

--- src/flat/BookACall/index.tsx
+++ src/flat/BookACall/index.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@nextui-org/react";
 
 function BookACall() {
+  function onClick() {
+    window.open("https://calendly.com/dimitrisstefanakis1/30min", "_blank");
+  }
+
   return (
-    <Button color="gradient" auto css={{ mr: "$xs" }}>
+    <Button color="gradient" auto css={{ mr: "$xs" }} onClick={onClick}>
       Book a Call
     </Button>
   );
